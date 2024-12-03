36ed1129d659fd5eee21a1a8211ad0f54b15086d
Mayaahmedd <maymoyaahmed@icloud.com>
2024-12-03 22:35:14 +0200

    Add files via upload

---
+++ SqlQuery_2.sql
@@ -0,0 +1,7 @@
+﻿
+
+INSERT INTO Customer_profile
+VALUES (1234 , 'ALI' , 'AHMED' , 'MAYA@GUC' ,'MAADI', '1/1/2004');
+INSERT INTO Customer_Account
+VALUES ('01019411877' ,'MAYA' , 499, 'Prepaid','1/1/2009','active' ,90 , 1234);
+
